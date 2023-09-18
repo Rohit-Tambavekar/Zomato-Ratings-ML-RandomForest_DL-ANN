2239e048fbdaec5ad493490abf0623b2d0f0a52f
Rohit Tambavekar <129712399+Rohit-Tambavekar@users.noreply.github.com>
2023-09-19 03:30:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,221 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+from model_fit import ZomatoModel
+from model_fit import *
+
+st.set_page_config(
+    page_title="Zomato Rating",
+    layout="wide",
+    page_icon=":food:",
+    menu_items={
+        'About': "Created By Rohit Tambavekar 'https://www.linkedin.com/in/rohit-tambavekar/'"
+    }
+)
+
+
+
+hide_st_style = """
+<style>
+header { visibility: hidden; }
+footer { visibility: hidden; }
+</style>
+"""
+
+OnlineOrders = ["Yes", "No"]
+TableBooking = ["Yes", "No"]
+Location = ['BTM', 'HSR', 'Koramangala 5th Block', 'JP Nagar', 'Whitefield',
+       'Indiranagar', 'Jayanagar', 'Marathahalli', 'Bannerghatta Road',
+       'Electronic City', 'Bellandur', 'Koramangala 1st Block', 'Brigade Road',
+       'Koramangala 7th Block', 'Koramangala 6th Block', 'Sarjapur Road',
+       'Ulsoor', 'Koramangala 4th Block', 'Banashankari', 'MG Road',
+       'Kalyan Nagar', 'Richmond Road', 'Frazer Town', 'Malleshwaram',
+       'Basavanagudi', 'Residency Road', 'New BEL Road', 'Brookefield',
+       'Kammanahalli', 'Banaswadi', 'Rajajinagar', 'Church Street',
+       'Lavelle Road', 'Shanti Nagar', 'Shivajinagar', 'Cunningham Road',
+       'Domlur', 'Old Airport Road', 'Ejipura', 'Commercial Street',
+       'St. Marks Road', 'Koramangala 8th Block', 'Vasanth Nagar',
+       'Jeevan Bhima Nagar', 'Wilson Garden', 'Bommanahalli',
+       'Koramangala 3rd Block', 'Kumaraswamy Layout', 'Thippasandra',
+       'Basaveshwara Nagar', 'Nagawara', 'Seshadripuram', 'Hennur',
+       'HBR Layout', 'Infantry Road', 'Majestic', 'Race Course Road',
+       'Yeshwantpur', 'City Market', 'ITPL Main Road, Whitefield',
+       'Koramangala 2nd Block', 'South Bangalore',
+       'Varthur Main Road, Whitefield', 'Kaggadasapura', 'Hosur Road',
+       'CV Raman Nagar', 'Sanjay Nagar', 'RT Nagar', 'Vijay Nagar',
+       'Sadashiv Nagar', 'Sahakara Nagar', 'Koramangala', 'Jalahalli',
+       'Magadi Road', 'East Bangalore', 'Rammurthy Nagar', 'Sankey Road',
+       'Langford Town', 'Old Madras Road', 'Mysore Road', 'Uttarahalli',
+       'KR Puram', 'Kanakapura Road', 'Hebbal', 'North Bangalore',
+       'Nagarbhavi', 'Kengeri', 'Central Bangalore', 'West Bangalore',
+       'Jakkur', 'Yelahanka', 'Peenya', 'Rajarajeshwari Nagar']
+RestaurantType = ['Casual Dining', 'Quick Bites', 'Cafe', 'Delivery',
+       'Dessert Parlor', 'Bakery', 'Takeaway','Delivery',
+       'Casual Dining', 'Bar', 'others']
+Cuisines = ['North Indian, Mughlai, Chinese',
+       'South Indian, North Indian', 'North Indian', 'Cafe',
+       'Cafe, Continental', 'Cafe, Fast Food', 'Cafe, Bakery',
+       'Bakery, Desserts', 'Pizza', 'Biryani',
+       'North Indian, Chinese, Fast Food', 'Chinese, Thai, Momos',
+       'South Indian', 'Burger, Fast Food', 'Pizza, Fast Food',
+       'North Indian, Chinese', 'Chinese, Thai', 'Ice Cream, Desserts',
+       'Biryani, Fast Food', 'Fast Food, Burger', 'Desserts, Beverages',
+       'Chinese', 'Bakery', 'Biryani, South Indian', 'Fast Food',
+       'South Indian, Chinese, North Indian', 'Mithai, Street Food',
+       'South Indian, Chinese', 'Biryani, North Indian, Chinese',
+       'Desserts', 'Ice Cream', 'South Indian, North Indian, Chinese',
+       'South Indian, Biryani', 'Beverages', 'Mithai',
+       'North Indian, Street Food', 'Chinese, North Indian',
+       'South Indian, North Indian, Chinese, Street Food', 'Andhra',
+       'Italian, Pizza', 'Street Food', 'Arabian',
+       'North Indian, Chinese, Continental', 'Desserts, Ice Cream',
+       'North Indian, Chinese, Biryani', 'Fast Food, Rolls',
+       'Beverages, Fast Food', 'North Indian, Chinese, South Indian',
+       'South Indian, Fast Food', 'North Indian, Fast Food',
+       'Beverages, Desserts', 'North Indian, Continental',
+       'North Indian, South Indian', 'North Indian, Biryani',
+       'Finger Food', 'Continental', 'Fast Food, Beverages',
+       'Andhra, Biryani', 'Biryani, Kebab', 'North Indian, Mughlai',
+       'North Indian, South Indian, Chinese', 'Cafe, Desserts',
+       'Biryani, North Indian', 'Chinese, Momos', 'Kerala, South Indian',
+       'Desserts, Bakery', 'Bakery, Fast Food', 'Kerala',
+       'North Indian, Chinese, Seafood', 'others']
+Type = ['Delivery', 'Drinks & nightlife', 'Dine-out', 'Cafes', 'Desserts',
+       'Buffet', 'Pubs and bars']
+
+
+def main():
+    # Create an instance of the ZomatoModel class
+    zomato_model = ZomatoModel()
+    train = pd.read_csv('zomato_train.csv')
+    test = pd.read_csv('zomato_test.csv')
+    # Load the trained model from the pickled file
+    zomato_model.unpickle_model()
+    zomato_model.unpickle_encodings()
+    zomato_model.unpickle_ann_model()
+    left_tit,center_tit ,right_tit = st.columns([15,10,15])
+    with center_tit:
+        st.title(":red[Zomato Ratings]")
+    selected = option_menu(
+        menu_title=None,
+        options = ["Home","Dashboard","Contact"],
+        icons=["house","kaban","envelope"],
+        menu_icon = "cast",
+        default_index = 0,
+        orientation = "horizontal"
+    )
+    
+    if selected == "Home":
+        col1, col2,col3 = st.columns([5,5,5])
+        
+        with col1:
+            online_order = st.selectbox("Online Orders:",OnlineOrders)
+            table_booking = st.selectbox("Table Booking :",TableBooking)
+            location = st.selectbox("Location :",Location)
+            restaurant_type = st.selectbox("Restaurant Type :",RestaurantType)
+            
+                 
+            
+        with col2:
+            cuisines = st.selectbox(" Cuisines :",Cuisines)
+            type = st.selectbox(" Restauranted listed in Type :",Type)
+            cost_for_two = st.number_input('Cost for two :',min_value = 60,max_value = 6000,value = 500,step = 10)
+            Votes = st.number_input('Votes :',min_value = 0,max_value = 6000,value = 100,step = 1)
+        
+        with col3:    
+            st.write("Check ratings for the Restaurant")
+            if st.button('Check Ratings'):
+                zomato_model.compute_location_freq_map(train)
+                ol_order,tb_book,loc_hotel,cui_hotel= zomato_model.input_encodings(online_order,table_booking,location,cuisines)
+                type_hotel = zomato_model.label_encoder.transform([type])[0]
+                # Votes,loc_hotel,cost_for_two,cui_hotel,ol_order,tb_book,type_hotel
+                data = {
+                        'votes': [Votes],
+                        'location': [loc_hotel],
+                        'cost_for_two': [cost_for_two],
+                        'cuisines': [cui_hotel],
+                        'online_order': [ol_order],
+                        'book_table': [tb_book],
+                        'type': [type_hotel]
+                    }
+                single_row_df = pd.DataFrame(data)
+
+                # Make predictions using the trained model
+                y_single_row_pred = zomato_model.rf.predict(single_row_df)[0]
+                st.write(f"The Random forest rating for the Restaurant is :{round(y_single_row_pred,1)}")     
+            
+            if st.button('Check ANN Ratings'):
+                zomato_model.compute_location_freq_map(train)
+                ol_order,tb_book,loc_hotel,cui_hotel= zomato_model.input_encodings(online_order,table_booking,location,cuisines)
+                type_hotel = zomato_model.label_encoder.transform([type])[0]
+                # Votes,loc_hotel,cost_for_two,cui_hotel,ol_order,tb_book,type_hotel
+                data_ann = {
+                        'votes': [Votes],
+                        'location': [loc_hotel],
+                        'cost_for_two': [cost_for_two],
+                        'cuisines': [cui_hotel],
+                        'online_order': [ol_order],
+                        'book_table': [tb_book],
+                        'type': [type_hotel]
+                    }
+                
+                # List of restaurant types
+                
+
+                # Create a list of keys with '_rest_type' suffix
+                restaurant_types_list = [f'{r_type}'+'_rest_type' for r_type in RestaurantType]
+                
+                # Append keys with values initialized to 0 to the data dictionary
+                for r_type in restaurant_types_list:
+                    data_ann[r_type] = [0]
+                data_ann[restaurant_type.replace(" ", "_")+'_rest_type'] = [1]
+                single_row_df = pd.DataFrame(data_ann)
+                desired_columns = [
+                                'online_order',
+                                'book_table',
+                                'votes',
+                                'location',
+                                'cuisines',
+                                'cost_for_two',
+                                'type',
+                                'Bakery_rest_type',
+                                'Bar_rest_type',
+                                'Cafe_rest_type',
+                                'Casual Dining_rest_type',
+                                'Delivery_rest_type',
+                                'Dessert Parlor_rest_type',
+                                'Quick Bites_rest_type',
+                                'Takeaway_rest_type',
+                                'others_rest_type'
+                            ]
+
+                # Reorder the columns
+                single_row_df = single_row_df[desired_columns]
+
+                # Sort the columns according to the desired order
+                
+                input_data = zomato_model.sc.transform(single_row_df)
+                # Then, make predictions using the ANN model
+                predictions = zomato_model.ann.predict(input_data)[0]
+                preds = np.round(predictions,1)
+                # Print the predictions
+                st.write(f'Ann Ratings for the Restaurant is : {np.round(preds,1)}')
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ model_fit.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
+from sklearn.ensemble import RandomForestRegressor as rfr
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+
+
+
+class ZomatoModel:
+
+    def __init__(self):
+        self.rf = None
+        self.ann = None
+        self.sc = None
+        self.mlb = MultiLabelBinarizer()
+        self.label_encoder = LabelEncoder()
+        self.order_book_map = {"No": 0, "Yes": 1}
+        self.columns_to_encode = ['cuisines', 'location']
+        self.sort_fimp = None
+        self.top_columns = None
+        self.traindf = None
+        self.testdf = None
+        self.le_file = None
+        self.loc_freq_map = None
+        self.cuisines_freq_map = None
+
+    def encode_online_book(self, train, test):
+        with open('online_order_mapping.pkl', 'wb') as mapping_order_table:
+            pickle.dump(self.order_book_map, mapping_order_table)
+
+        train['online_order'] = train['online_order'].map(self.order_book_map)
+        test['online_order'] = test['online_order'].map(self.order_book_map)
+
+        train['book_table'] = train['book_table'].map(self.order_book_map)
+        test['book_table'] = test['book_table'].map(self.order_book_map)
+
+    def encode_rest_type(self, train, test):
+        rest_type_df = pd.DataFrame(self.mlb.fit_transform(train['rest_type'].str.split(', ')),
+                                    columns=self.mlb.classes_, index=train.index)
+        rest_type_df = rest_type_df.add_suffix('_rest_type')
+
+        rest_type_test_df = pd.DataFrame(self.mlb.transform(test['rest_type'].str.split(', ')),
+                                         columns=self.mlb.classes_, index=test.index)
+        rest_type_test_df = rest_type_test_df.add_suffix('_rest_type')
+
+        train = train.join(rest_type_df)
+        test = test.join(rest_type_test_df)
+        return train,test
+
+    def compute_location_freq_map(self, train):
+        self.loc_freq_map = train['location'].value_counts().to_dict()
+        self.cuisines_freq_map = train['cuisines'].value_counts().to_dict()
+        return self.loc_freq_map, self.cuisines_freq_map
+    
+    def freq_encode_columns(self, train, test):
+        for column in self.columns_to_encode:
+            if column == 'location':
+                train[column] = train[column].map(self.loc_freq_map)
+                test[column] = test[column].map(self.loc_freq_map)
+                
+            if column == 'cuisines':
+                train[column] = train[column].map(self.cuisines_freq_map)
+                test[column] = test[column].map(self.cuisines_freq_map)
+   
+    def label_encode_type(self, train, test):
+        train['type'] = self.label_encoder.fit_transform(train['type'])
+        test['type'] = self.label_encoder.fit_transform(test['type'])
+
+    def drop_columns(self, train, test):
+        train.drop(['rest_type', 'name'], axis=1, inplace=True)
+        test.drop(['rest_type', 'name'], axis=1, inplace=True)
+
+    def train_and_evaluate_model(self, train, test):
+        X = train.drop(['rate'], axis=1)
+        y = train['rate']
+        x_train, x_eval, y_train, y_eval = train_test_split(X, y, random_state=2, test_size=0.2, stratify=train.rate)
+
+        self.rf = rfr(criterion='squared_error', max_depth=30, min_samples_split=2, n_estimators=150)
+        self.rf.fit(x_train, y_train)
+
+        pred = self.rf.predict(x_eval)
+        y_test_pred = self.rf.predict(test.drop(['rate'], axis=1))
+
+        return x_train
+
+    def get_feature_importance(self,x_train):
+        if self.rf is not None:
+            print(x_train.columns)
+            fimp = pd.Series(self.rf.feature_importances_, index=x_train.columns)
+            self.sort_fimp = fimp.sort_values(ascending=False)
+            return self.sort_fimp
+        else:
+            return None
+        
+    def train_model_on_ft_importance(self,sort_fimp,train,test):
+        self.top_columns = list(sort_fimp.head(7).index)
+        
+        # Convert the index (self.top_columns) to a list of column names
+        # self.top_columns = train.columns[self.top_columns]
+
+        # Keep only the top columns in the train dataset
+        self.traindf = train[self.top_columns].copy()  # Create a copy to avoid the warning
+        # Add the 'rate' column back to the train dataset
+        self.traindf['rate'] = train['rate'].copy()  # Create a copy to avoid the warning
+
+        # Keep only the top columns in the test dataset
+        self.testdf = test[self.top_columns].copy()  # Create a copy to avoid the warning
+        # Add the 'rate' column back to the test dataset
+        self.testdf['rate'] = test['rate'].copy()  # Create a copy to avoid the warning
+        pd.set_option('display.max_columns',None)
+                
+        X1 = self.traindf.drop(['rate'], axis=1)
+        y1 = self.traindf['rate']
+        x_train, x_eval, y_train, y_eval = train_test_split(X1, y1, random_state=2, test_size=0.2, stratify=y1)
+        
+        self.rf = rfr(criterion = 'squared_error', max_depth= 30, min_samples_split= 2, n_estimators= 150)
+        self.rf.fit(x_train, y_train)
+
+        pred = self.rf.predict(x_eval)
+    
+    def pickle_encodings(self):
+        with open('label_encoder.pkl', 'wb') as le_file:
+            pickle.dump(self.label_encoder, le_file)
+
+        with open('multi_label_binarizer.pkl', 'wb') as mlb_file:
+            pickle.dump(self.mlb, mlb_file)
+
+    def pickle_model(self):
+        if self.rf is not None:
+            with open('random_forest_model.pkl', 'wb') as model_file:
+                pickle.dump(self.rf, model_file)
+    
+    def unpickle_model(self):
+        try:
+            with open('random_forest_model.pkl', 'rb') as model_file:
+                self.rf = pickle.load(model_file)
+        except FileNotFoundError:
+            print("Model file not found. Please make sure 'random_forest_model.pkl' exists.")
+
+    def unpickle_encodings(self):
+        try:
+            with open('label_encoder.pkl', 'rb') as le_file:
+                self.label_encoder = pickle.load(le_file)
+
+            with open('multi_label_binarizer.pkl', 'rb') as mlb_file:
+                self.mlb = pickle.load(mlb_file)
+
+            with open('online_order_mapping.pkl', 'rb') as mapping_order_table:
+                self.order_book_map = pickle.load(mapping_order_table)
+
+        except FileNotFoundError:
+            print("One or more encoding files not found. Make sure the necessary files exist.")
+
+    def input_encodings(self,online_order,table_booking,location,cuisines): 
+        online_order = self.order_book_map[online_order]
+        table_booking = self.order_book_map[table_booking]
+        location = self.loc_freq_map[location]
+        cuisines = self.cuisines_freq_map[cuisines]
+        return online_order,table_booking, location,cuisines
+    
+    def train_ann_model(self,train,test):
+        X = train.drop(['rate'], axis=1)
+        y = train['rate']
+        
+        x_train,x_eval,y_train,y_eval = train_test_split(X,y, random_state=2,test_size=0.2,stratify=train.rate)
+        
+        self.sc = StandardScaler()
+        X_train = self.sc.fit_transform(x_train)
+        X_eval = self.sc.transform(x_eval)
+        pd.set_option('display.max_columns',None)
+        print(x_train.head(2))
+        
+        self.ann = tf.keras.models.Sequential()
+        self.ann.add(tf.keras.layers.Dense(units=4, activation='tanh'))
+        self.ann.add(tf.keras.layers.Dense(units=1, activation='tanh'))
+        self.ann.add(tf.keras.layers.Dense(units=1))
+        self.ann.compile(optimizer = 'sgd', loss = 'mean_squared_error')
+        self.ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+        
+    def pickle_ann_model(self):
+        try:
+            with open('ann_model.pkl', 'wb') as ann_model_file:
+                pickle.dump(self.ann, ann_model_file)
+                pickle.dump(self.sc, ann_model_file)  # Pickle the StandardScaler too
+            print(f"ANN model and StandardScaler have been pickled and saved successfully.")
+        except Exception as e:
+            print(f"An error occurred while pickling the ANN model: {str(e)}")
+
+    def unpickle_ann_model(self):
+        try:
+            with open('ann_model.pkl', 'rb') as ann_model_file:
+                self.ann = pickle.load(ann_model_file)
+                self.sc = pickle.load(ann_model_file)  # Unpickle the StandardScaler too
+            print(f"ANN model and StandardScaler have been successfully unpickled.")
+        except FileNotFoundError:
+            print("ANN model file 'ann_model.pkl' not found. Please make sure it exists.")
+        except Exception as e:
+            print(f"An error occurred while unpickling the ANN model: {str(e)}")
+                
+        
+        
+        
+def main():
+    train = pd.read_csv('zomato_train.csv')
+    test = pd.read_csv('zomato_test.csv')
+    pd.set_option('display.max_rows',None)
+        
+    zomato_model = ZomatoModel()
+
+    zomato_model.encode_online_book(train, test)
+    train,test = zomato_model.encode_rest_type(train, test)
+    zomato_model.compute_location_freq_map(train)
+    zomato_model.freq_encode_columns(train, test)
+    zomato_model.label_encode_type(train, test)
+    zomato_model.drop_columns(train, test)
+    
+    x_train = zomato_model.train_and_evaluate_model(train, test)
+    zomato_model.train_ann_model(train,test)
+    
+    sort_fimp = zomato_model.get_feature_importance(x_train)
+    zomato_model.train_model_on_ft_importance(sort_fimp,train,test)
+    
+    zomato_model.pickle_encodings()
+    zomato_model.pickle_ann_model()
+    zomato_model.pickle_model()
+
+if __name__ == "__main__":
+    main()
